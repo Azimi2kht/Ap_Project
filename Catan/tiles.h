31dd28b94124b59d0063c60be2bec3ab1b4acca1
MrVein <s.h.azimidokht80@gmail.com>
2021-07-04 18:14:05 +0430

    Add UML attributes to Tiles clas

---
+++ Catan/tiles.h
@@ -0,0 +1,16 @@
+#ifndef TILES_H
+#define TILES_H
+
+
+class Tiles
+{
+private:
+    char placement_permision_side[6];
+    int placement_permision_tip[6];
+
+public:
+    Tiles();
+
+};
+
+#endif // TILES_H
