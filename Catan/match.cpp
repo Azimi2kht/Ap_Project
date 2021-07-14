655b1f8a0b316877bc2239ae51f77b5472314000
MrVein <s.h.azimidokht80@gmail.com>
2021-07-14 15:47:16 +0430

    Replace server project

---
+++ Catan/match.cpp
@@ -0,0 +1,20 @@
+#include "match.h"
+
+#include <iostream>
+#include <algorithm>
+
+Match::Match() {
+
+}
+
+    // add parameter for players dice values. comes from Players; TODO
+void Match::chooseFirst() {
+    // sort the values:
+    //std::sort(input.begin(), input.end());
+
+    // iterate the values to set turns
+
+
+    // iterate the values reverse to set turns
+
+}
---
+++ Catan/match.h
@@ -0,0 +1,19 @@
+#ifndef MATCH_H
+#define MATCH_H
+
+#include "dice.h"
+
+class Match
+{
+public:
+    Match();
+
+
+
+
+private:
+    int playerNum;
+
+};
+
+#endif // MATCH_H
